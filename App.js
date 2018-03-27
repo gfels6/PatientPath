8cc046725a59bd0869ee6a4e38b898a2c62c91d6
kybup1 <philip.kyburz@students.bfh.ch>
2018-03-27 17:47:10 +0200

    change to a ugly color

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f24',
     alignItems: 'center',
     justifyContent: 'center',
   },
